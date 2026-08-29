178a3e2c46c9974e54de6185f7b1c38117428ee7
ObiWanKenobi <metal.rulesall@gmail.com>
2026-08-29 12:54:55 +0200

    fix: custom css not exported
    - rename to `.css`, we don't need sass
    - re export from `custom/index.ts`

--- src/ui/custom/index.ts
+++ src/ui/custom/index.ts
@@ -4,6 +4,7 @@ import Tab from "./Tab.svelte"
 import GoogleDriveConnectModal from "./GoogleDriveConnectModal.svelte"
 import ConfirmModal from "./ConfirmModal.svelte"
 import UserPasswordModal from "./UserPasswordModal.svelte"
+import "../main.css"
 
 export {
     HeaderWithIcon,
